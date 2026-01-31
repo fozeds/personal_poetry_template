e6644fe277ed888a8d77c4689c3047acb0b845a4
Fozeds <sem_email_publico@por_enquanto.com.br>
2026-01-31 16:29:07 -0300

    fix: fix hooks exit code

--- scripts/git-hooks/pre-commit.ps1
+++ scripts/git-hooks/pre-commit.ps1
@@ -1,6 +1,6 @@
 # Executa o script que bloqueia commits na main ou master
-py scripts/block_branch.py
+py scripts/block_branch.py || exit 1
 
 # Se não bloqueado, adiciona os arquivos modificados
-py scripts/add_repo_path_header.py
+py scripts/add_repo_path_header.py || exit 1
 git add -u
